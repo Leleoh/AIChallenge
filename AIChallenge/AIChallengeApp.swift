d4ef99e09a3bf26877e28cb481010c003f74a8f5
Leonel <leonelfhernandez@outlook.com>
2026-07-14 16:30:21 -0300

    feat: commit inicial com gitignore

---
+++ AIChallenge/AIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChallengeApp.swift
+//  AIChallenge
+//
+//  Created by Leonel Ferraz Hernandez on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIChallenge
+//
+//  Created by Leonel Ferraz Hernandez on 14/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
